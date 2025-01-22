5bf315aba12f1388efc4a5abdf405fc77b4c9e32
Jakub Pałka <jakub.palka97@gmail.com>
2025-01-22 11:06:07 +0100

    Deleted packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,10 @@
 'use client'
-
-import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs'
 import UrlShortnerContainer from '@/components/url-shortner-container'
 
 export default function Home() {
 	return (
 		<>
-			{/* Jeśli użytkownik jest zalogowany, pokaż główny komponent */}
-			<SignedIn>
-				<UrlShortnerContainer />
-			</SignedIn>
-
-			{/* Jeśli użytkownik nie jest zalogowany, przekieruj na stronę logowania Clerk */}
-			<SignedOut>
-				<RedirectToSignIn />
-			</SignedOut>
+			<UrlShortnerContainer />
 		</>
 	)
 }
